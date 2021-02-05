cefee52809cc0f17e09fa16c4ed532b46bd50df2
Oli Lane <78598586+OliLane@users.noreply.github.com>
2021-02-05 16:51:59 +0000

    Update dice_roller.py
    Asks user how many dice to roll. Asks the user of the number of sides the dice should have. Informs the user of the results of each die roll.

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,22 @@
 import random
-dice_rolls = 2
+dice_rolls = int(input('How many dice would you like to roll?' ))
+dice_size = int(input('How many sides do the dice have?' ))
 dice_sum = 0
 for i in range(0,dice_rolls):
-  roll = random.randint(1,6)
+  roll = random.randint(1,dice_size)
   dice_sum = dice_sum + roll
+  if roll == 1:
+    print(f'You rolled a {roll}... Critical Fail!')
+  elif roll == dice_size:
+      print(f'You rolled a {roll}. Critical Success!')
+  else:
+        print(f'You rolled a {roll}')
+  
 def main():
   print('You rolled two dice')
+  print(f'you rolled a total of {dice_sum}')
   
-  print(f'You rolled a total of {dice_sum}')
+
 
 if __name__== "__main__":
   main()
